a4e5ef790ee7028b354af328d7143230f229bed5
Tamiyo22 <melissaeaybar@gmail.com>
2019-05-16 23:37:00 -0500

    react review todo list

---
+++ my-app/src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function About() {
+	return (
+		//ghost element if you don't want to wrap in a div
+		<React.Fragment>
+
+ <h1> About</h1>
+
+ <p> Melissa's React Review! Second time using React Router! </p>
+
+
+		</React.Fragment>
+	)
+}
